9f73489b53bce430098d6700be329913c37a5b8c
Michael McQuade <michael@giraffesyo.io>
2021-06-26 05:13:25 -0500

    Add save button - Can save output to file

--- components/Output.tsx
+++ components/Output.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react'
+import { FaSave } from 'react-icons/fa'
 import type { ISection, ISectionState } from '../pages'
 
 interface IOutputComponentProps {
@@ -39,8 +41,40 @@ const OutputComponent: React.FC<IOutputComponentProps> = ({ sections }) => {
   //     sections.required.aws,
   //     sections.required.cluster,
   //   ]
+  const OutputDiv = useRef(null)
+
+  const handleSave = () => {
+    const text = (OutputDiv.current as unknown as HTMLDivElement).innerText
+    const filename = 'parallelcluster.ini'
+    const file = new Blob([text], { type: 'text' })
+    if (window.navigator.msSaveOrOpenBlob)
+      // IE10+
+      window.navigator.msSaveOrOpenBlob(file, filename)
+    else {
+      // Others
+      let a = document.createElement('a'),
+        url = URL.createObjectURL(file)
+      a.href = url
+      a.download = filename
+      document.body.appendChild(a)
+      a.click()
+      setTimeout(function () {
+        document.body.removeChild(a)
+        window.URL.revokeObjectURL(url)
+      }, 0)
+    }
+  }
   return (
-    <div className='bg-gray-400 font-mono fixed w-1/4 overflow-y-scroll h-3/4'>
+    <div
+      ref={OutputDiv}
+      className=' bg-gray-400 font-mono fixed w-1/4 overflow-y-scroll h-3/4'
+    >
+      <div
+        onClick={handleSave}
+        className='absolute text-blue-900 text-4xl top-0 right-0 mr-1 mt-1'
+      >
+        <FaSave className='hover:opacity-80 cursor-pointer'></FaSave>
+      </div>
       {sections.map((section, index) => (
         <div key={`section-${section.section_name}`}>
           <div className='lowercase'>{determineSectionName(section)}</div>
